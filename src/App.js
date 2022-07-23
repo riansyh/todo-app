e25252a3de9d03fdb59ec4c3ca2ef792a9f6312f
Rian Febriansyah <rianfebriansyah22@gmail.com>
2022-07-23 22:22:50 +0700

    feat: remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import React, { useReducer } from "react";
+import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
-import MainRTK from "./components/MainRTK";
-import { initialState, reducer } from "./utils/reducer";
-
-export const TasksContext = React.createContext();
 
 function App() {
-    const [tasks, dispatch] = useReducer(reducer, initialState);
-
     return (
-        <TasksContext.Provider value={{ state: tasks, dispatch }}>
-            <div className="container">
-                <Header />
-                <MainRTK />
-            </div>
-        </TasksContext.Provider>
+        <div className="container">
+            <Header />
+            <Main />
+        </div>
     );
 }
 export default App;
